1582e4261d850cec2174aaeceea687ca2b9aacbd
Devil <singhsansar433@gmail.com>
2023-04-08 04:28:50 +0530

    model working

--- SnakeAI/agent.py
+++ SnakeAI/agent.py
@@ -23,12 +23,16 @@ class Agent:
 
     # finding all the state of the game 
     def get_state(self, game):
-        head = game.snake[0]
+
+        head = game.snake[0] #point the snake head
+
+        #position of the snake head
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
         
+        #state of the direction 
         dir_l = game.direction == Direction.LEFT
         dir_r = game.direction == Direction.RIGHT
         dir_u = game.direction == Direction.UP
@@ -66,7 +70,7 @@ class Agent:
             game.food.y > game.head.y  # food down
             ]
 
-        return np.array(state, dtype=int)
+        return np.array(state, dtype=int) #[danger staraight, danger-right, dangerleft , move directions , food locations]
 
 
     def remember(self, state, action, reward, next_state, done):
@@ -93,59 +97,68 @@ class Agent:
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
+
+            #now start predecting the states 
         else:
+            #A torch.Tensor is a multi-dimensional matrix containing elements of a single data type.
             state0 = torch.tensor(state, dtype=torch.float)
             prediction = self.model(state0)
+            #Returns the indices of the maximum value of all elements in the input tensor
             move = torch.argmax(prediction).item()
             final_move[move] = 1
-
         return final_move 
 
 
 #Training the model 
 def train():
-    plot_scores = []
-    plot_mean_scores = []
-    total_score = 0
-    record = 0
-    agent = Agent()
-    game = SnakeGameAI()
+    plot_scores = [] #scores to be ploted 
+    plot_mean_scores = [] #mean of the scores  line 
+    total_score = 0 #total score 
+    record = 0 #a recored of scores 
+    agent = Agent() #initialize agent class 
+    game = SnakeGameAI() #initialize the game class 
     while True:
+
         # get old state
-        state_old = agent.get_state(game)
+        state_old = agent.get_state(game)  #get the initial state of the game 
 
         # get move
-        final_move = agent.get_action(state_old)
+        final_move = agent.get_action(state_old) #get the move for that state 
 
         # perform move and get new state
-        reward, done, score = game.play_step(final_move)
-        state_new = agent.get_state(game)
+        reward, done, score = game.play_step(final_move) #perform that move in the game 
+        state_new = agent.get_state(game) #get a new state after performing the move
 
         # train short memory
-        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        agent.train_short_memory(state_old, final_move, reward, state_new, done) #training the short memory 
 
         # remember
-        agent.remember(state_old, final_move, reward, state_new, done)
+        agent.remember(state_old, final_move, reward, state_new, done) #remember the moves and states 
 
+        #when game is finished
         if done:
             # train long memory, plot result
-            game.reset()
-            agent.n_games += 1
-            agent.train_long_memory()
+            game.reset() #reset the game 
+            agent.n_games += 1 #add one in the number of games played 
+            agent.train_long_memory() #save the data in the long train in the data stracture, final data after game completed 
 
+
+            #if new record is game , (high score), save the model state 
             if score > record:
                 record = score
+                #save the model 
                 agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
+            #updates the result
             plot_scores.append(score)
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
 
+
+#starting from training the model 
 if __name__ =='__main__':
     train()
-
-   
--- SnakeAI/model.py
+++ SnakeAI/model.py
@@ -6,10 +6,12 @@ import os
 
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
+        # make a model with the input hidden and outputlayer 
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
 
+    #used the relu activation function of the deep learining model 
     def forward(self, x):
         x = F.relu(self.linear1(x))
         x = self.linear2(x)
@@ -24,13 +26,14 @@ class Linear_QNet(nn.Module):
         torch.save(self.state_dict(), file_name)
 
 
+#Mathematics behind the model
 class QTrainer:
     def __init__(self, model, lr, gamma):
         self.lr = lr
         self.gamma = gamma
         self.model = model
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-        self.criterion = nn.MSELoss()
+        self.criterion = nn.MSELoss() #mean-squared error 
 
     def train_step(self, state, action, reward, next_state, done):
         state = torch.tensor(state, dtype=torch.float)
@@ -39,8 +42,10 @@ class QTrainer:
         reward = torch.tensor(reward, dtype=torch.float)
         # (n, x)
 
+        #handeling both single and multiple data 
         if len(state.shape) == 1:
             # (1, x)
+            # Returns a new tensor with a dimension of size one inserted at the specified position.
             state = torch.unsqueeze(state, 0)
             next_state = torch.unsqueeze(next_state, 0)
             action = torch.unsqueeze(action, 0)
@@ -49,18 +54,21 @@ class QTrainer:
 
        # 1: predicted Q values with current state
         pred = self.model(state)
+        target = pred.clone() #clone() function makes an exact copy of the original image.
 
-        target = pred.clone()
+        
+        #updating the Q-new with the previous state 
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx])) #formula for the Q learning 
 
             target[idx][torch.argmax(action[idx]).item()] = Q_new
     
         # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
         # pred.clone()
         # preds[argmax(action)] = Q_new
+        
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
